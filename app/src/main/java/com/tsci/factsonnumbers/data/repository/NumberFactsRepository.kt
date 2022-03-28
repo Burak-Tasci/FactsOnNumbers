795f99b6d9477d08e9a917483e1628af981de756
Burak-Tasci <buraaktascii@outlook.com>
2022-03-29 00:59:18 +0300

    Add repository

---
+++ app/src/main/java/com/tsci/factsonnumbers/data/repository/NumberFactsRepository.kt
@@ -0,0 +1,19 @@
+package com.tsci.factsonnumbers.data.repository
+
+
+interface NumberFactsRepository {
+
+    suspend fun getTriviaInfoByNumber(number: String): String
+
+    suspend fun getMathInfoByNumber(number: String): String
+
+    suspend fun getDateInfoByNumber(month: String, day: String): String
+
+    suspend fun getRandomTriviaInfo(): String
+
+    suspend fun getRandomMathInfo(): String
+
+    suspend fun getRandomDateInfo(): String
+
+    suspend fun getRandomYearInfo(): String
+}
