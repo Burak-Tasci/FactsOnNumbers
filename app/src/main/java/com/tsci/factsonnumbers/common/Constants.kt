b1c0cd24901dbb9c14d5681512d13ab108b1c8b9
Burak-Tasci <buraaktascii@outlook.com>
2022-03-29 00:41:26 +0300

    Add base url for api

--- app/src/main/java/com/tsci/factsonnumbers/common/Constants.kt
+++ app/src/main/java/com/tsci/factsonnumbers/common/Constants.kt
@@ -2,6 +2,5 @@ package com.tsci.cryptocurrencyapp.common
 
 object Constants {
 
-    const val BASE_URL = "https://api.coinpaprika.com/"
-    const val PARAM_COIN_ID = "coinId"
+    const val BASE_URL = "http://numbersapi.com/"
 }
