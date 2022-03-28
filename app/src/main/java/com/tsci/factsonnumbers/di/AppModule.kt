795e3efaae162de78ab6ccac322ff6b618f80522
Burak-Tasci <buraaktascii@outlook.com>
2022-03-29 00:45:50 +0300

    Add AppModule.kt

---
+++ app/src/main/java/com/tsci/factsonnumbers/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.tsci.factsonnumbers.di
+
+import com.tsci.cryptocurrencyapp.common.Constants
+import com.tsci.factsonnumbers.data.remote.NumberFactsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNumberFactsApi(): NumberFactsApi{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NumberFactsApi::class.java)
+    }
+}
