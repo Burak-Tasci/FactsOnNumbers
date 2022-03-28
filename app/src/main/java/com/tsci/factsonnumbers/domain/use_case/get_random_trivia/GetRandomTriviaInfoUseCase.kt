f02be960e1190c44131de10e96140c51941965fc
Burak-Tasci <buraaktascii@outlook.com>
2022-03-29 01:24:18 +0300

    Add use cases

---
+++ app/src/main/java/com/tsci/factsonnumbers/domain/use_case/get_random_trivia/GetRandomTriviaInfoUseCase.kt
@@ -0,0 +1,25 @@
+package com.tsci.factsonnumbers.domain.use_case.get_random_trivia
+
+import com.tsci.factsonnumbers.common.Resource
+import com.tsci.factsonnumbers.data.repository.NumberFactsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetRandomTriviaInfoUseCase @Inject constructor(
+    private val repository: NumberFactsRepository
+){
+    operator fun invoke(): Flow<Resource<String>> = flow {
+        try {
+            emit(Resource.Loading())
+            val info = repository.getRandomTriviaInfo()
+            emit(Resource.Success(info))
+        }catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage?: "An unexpected error occured!"))
+        }catch (e: IOException){
+            emit(Resource.Error("Could not reach server, check your internet connection."))
+        }
+    }
+}
