fa83623c80e00813255aa3861d6ba94430d5bf5f
Burak-Tasci <buraaktascii@outlook.com>
2022-04-01 01:37:02 +0300

    add scalar response converter to api

--- app/src/main/java/com/tsci/factsonnumbers/di/AppModule.kt
+++ app/src/main/java/com/tsci/factsonnumbers/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.tsci.factsonnumbers.di
 
+import android.content.Context
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.core.content.ContextCompat
 import com.tsci.cryptocurrencyapp.common.Constants
 import com.tsci.factsonnumbers.data.remote.NumberFactsApi
 import com.tsci.factsonnumbers.data.repository.NumberFactsRepository
@@ -7,22 +10,26 @@ import com.tsci.factsonnumbers.data.repository.NumberFactsRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.Headers
 import javax.inject.Singleton
-/*
-    Dependecy injection module
- */
+
+
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
+    @Headers("Content-Type: text/html")
     @Singleton
     fun provideNumberFactsApi(): NumberFactsApi{
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(NumberFactsApi::class.java)
@@ -32,4 +39,7 @@ object AppModule {
     fun provideNumberFactsRepository(api: NumberFactsApi): NumberFactsRepository{
         return NumberFactsRepositoryImpl(api)
     }
+
+
+
 }
