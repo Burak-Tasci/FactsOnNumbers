36868feb8de0d0e7a95e655f8cd41e666316cd59
Burak-Tasci <buraaktascii@outlook.com>
2022-04-01 01:34:31 +0300

    inject UseCases.kt to constructor and add a test method to check app flow

--- app/src/main/java/com/tsci/factsonnumbers/presentation/facts_options/OptionsListViewModel.kt
+++ app/src/main/java/com/tsci/factsonnumbers/presentation/facts_options/OptionsListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tsci.factsonnumbers.common.Resource
 import com.tsci.factsonnumbers.domain.use_cases.UseCases
+import com.tsci.factsonnumbers.presentation.fact_detail.FactDetailState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -17,28 +18,9 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class OptionsListViewModel @Inject constructor(
-    private val useCases: UseCases
+
 ): ViewModel(){
 
-    private val _state = mutableStateOf(OptionsListState())
-    val state: State<OptionsListState> = _state
 
-
-    fun getTriviaInfoByNumber(number: String){
-        useCases.GetTriviaInfoUseCase().invoke(number = number).onEach { result ->
-            when(result){
-
-                is Resource.Loading -> {
-                    _state.value = OptionsListState(isLoading = true)
-                }
-                is Resource.Success -> {
-                    _state.value = OptionsListState(fact = result.toString())
-                }
-                is Resource.Error -> OptionsListState(
-                    error = result.message ?: "An unexpected error occurred"
-                )
-            }
-        }.launchIn(viewModelScope)
-    }
 
 }
