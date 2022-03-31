e5fe3f65496f9744e5799b202a494a4498432c96
Burak-Tasci <buraaktascii@outlook.com>
2022-04-01 01:34:51 +0300

    inject UseCases.kt to constructor and add a test method to check data flow

--- app/src/main/java/com/tsci/factsonnumbers/presentation/fact_detail/FactDetailViewModel.kt
+++ app/src/main/java/com/tsci/factsonnumbers/presentation/fact_detail/FactDetailViewModel.kt
@@ -1,11 +1,38 @@
 package com.tsci.factsonnumbers.presentation.fact_detail
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.tsci.factsonnumbers.data.repository.NumberFactsRepository
+import androidx.lifecycle.viewModelScope
+import com.tsci.factsonnumbers.common.Resource
+import com.tsci.factsonnumbers.domain.use_cases.UseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@HiltViewModel
 class FactDetailViewModel @Inject constructor(
-    numberFactsRepository: NumberFactsRepository
+    private val useCases: UseCases
 ): ViewModel() {
+    private val _state = mutableStateOf(FactDetailState())
+    val state: State<FactDetailState> = _state
 
+
+    fun getTriviaInfoByNumber(number: String){
+        useCases.GetTriviaInfoUseCase().invoke(number = number).onEach { result ->
+            when(result){
+
+                is Resource.Loading -> {
+                    _state.value = FactDetailState(isLoading = true)
+                }
+                is Resource.Success -> {
+                    _state.value = FactDetailState(fact = result.data.toString())
+                }
+                is Resource.Error -> FactDetailState(
+                    error = result.message ?: "An unexpected error occurred"
+                )
+            }
+        }.launchIn(viewModelScope)
+    }
 }
