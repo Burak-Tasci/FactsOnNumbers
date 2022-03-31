ca97b0167538f45780087056de6ce07cbbb22913
Burak-Tasci <buraaktascii@outlook.com>
2022-04-01 01:38:59 +0300

    create enum class to direct api methods in a better way

---
+++ app/src/main/java/com/tsci/factsonnumbers/common/ApiWay.kt
@@ -0,0 +1,12 @@
+package com.tsci.factsonnumbers.common
+
+enum class ApiWay {
+    TRIVIA,
+    MATH,
+    DATE,
+    YEAR,
+    RANDOM_TRIVIA,
+    RANDOM_MATH,
+    RANDOM_DATE,
+    RANDOM_YEAR
+}
