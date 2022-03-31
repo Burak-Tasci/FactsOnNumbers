db64e9006f35ec7bca38f1f7d84bfd1f758d12f3
Burak-Tasci <buraaktascii@outlook.com>
2022-04-01 01:31:52 +0300

    update size

--- app/src/main/java/com/tsci/factsonnumbers/presentation/facts_options/components/OptionsListItem.kt
+++ app/src/main/java/com/tsci/factsonnumbers/presentation/facts_options/components/OptionsListItem.kt
@@ -1,12 +1,13 @@
 package com.tsci.factsonnumbers.presentation.facts_options.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -24,7 +25,8 @@ fun OptionsListItem(
         onClick = { onButtonClick() },
         Modifier
             .padding(8.dp)
-            .height(100.dp),
+            .height(100.dp)
+            .width(140.dp),
         shape = RoundedCornerShape(20.dp)
 
     ) {
@@ -32,7 +34,8 @@ fun OptionsListItem(
             text = itemText,
             textAlign = TextAlign.Center,
             modifier = Modifier
-                .padding(8.dp),
+                .padding(8.dp)
+                .wrapContentWidth(),
             fontFamily = FontFamily.Serif
         )
     }
