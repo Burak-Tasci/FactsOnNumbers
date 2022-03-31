afe0ce132671b76390e140751b831d812ee7105d
Burak-Tasci <buraaktascii@outlook.com>
2022-03-31 16:17:55 +0300

    create FactDetailViewModel.kt

---
+++ app/src/main/java/com/tsci/factsonnumbers/presentation/fact_detail/FactDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.tsci.factsonnumbers.presentation.fact_detail
+
+import androidx.lifecycle.ViewModel
+import com.tsci.factsonnumbers.data.repository.NumberFactsRepository
+import javax.inject.Inject
+
+class FactDetailViewModel @Inject constructor(
+    numberFactsRepository: NumberFactsRepository
+): ViewModel() {
+
+}
