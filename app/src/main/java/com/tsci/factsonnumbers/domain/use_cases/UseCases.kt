c6d458dacb09731d08c810047c5bd4d62a6c2991
Burak-Tasci <buraaktascii@outlook.com>
2022-04-02 01:10:47 +0300

    add use cases

--- app/src/main/java/com/tsci/factsonnumbers/domain/use_cases/UseCases.kt
+++ app/src/main/java/com/tsci/factsonnumbers/domain/use_cases/UseCases.kt
@@ -1,11 +1,10 @@
 package com.tsci.factsonnumbers.domain.use_cases
 
-import androidx.lifecycle.viewModelScope
+import android.util.Log
 import com.tsci.factsonnumbers.common.Resource
 import com.tsci.factsonnumbers.data.repository.NumberFactsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -22,10 +21,131 @@ class UseCases @Inject constructor(
             try {
                 emit(Resource.Loading())
                 val fact = repository.getTriviaInfoByNumber(number = number)
+                Log.d("UseCases", fact)
                 emit(Resource.Success(fact))
             } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
                 emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
             } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetMathInfoUseCase{
+        operator fun invoke(number: String): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getMathInfoByNumber(number = number)
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetDateInfoUseCase{
+        operator fun invoke(month: String, day: String): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getDateInfoByNumber(month = month, day = day)
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetYearInfoUseCase{
+        operator fun invoke(number: String): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getYearInfoByNumber(number = number)
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetRandomTriviaInfoUseCase{
+        operator fun invoke(): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getRandomTriviaInfo()
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetRandomMathInfoUseCase{
+        operator fun invoke(): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getRandomMathInfo()
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+    inner class GetRandomDateInfoUseCase{
+        operator fun invoke(): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getRandomDateInfo()
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error("Could not reach server, check your internet connection."))
+            }
+        }
+    }
+
+    inner class GetRandomYearInfoUseCase{
+        operator fun invoke(): Flow<Resource<String>> = flow {
+            try {
+                emit(Resource.Loading())
+                val fact = repository.getRandomYearInfo()
+                Log.d("UseCases", fact)
+                emit(Resource.Success(fact))
+            } catch (e: HttpException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured!"))
+            } catch (e: IOException) {
+                Log.d("UseCases", "invoke: ${e.localizedMessage}")
                 emit(Resource.Error("Could not reach server, check your internet connection."))
             }
         }
