7145b037f6cc90e0291742443e05320bab7bdf3e
Burak-Tasci <buraaktascii@outlook.com>
2022-04-03 14:23:43 +0300

    change theme colors

--- app/src/main/java/com/tsci/factsonnumbers/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/tsci/factsonnumbers/presentation/ui/theme/Theme.kt
@@ -5,17 +5,25 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF8187ff),
+    primaryVariant = Color(0XFF0031ca),
+    secondary = Color(0xFF757de8),
+    secondaryVariant = Color(0xFF3f51b5),
+    onBackground = Color(0xFF8187ff)
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200,
+    primary = Color(0xFF3d5afe),
+    primaryVariant = Color(0XFF0031ca),
+    secondary = Color(0xFF757de8),
+    secondaryVariant = Color(0xFF3f51b5) ,
+    onBackground = Color(0xFF3d5afe)
 
     /* Other default colors to override
     background = Color.White,
